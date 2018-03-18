1f5210adf08895cb326c296eb5ba3ca75a689a2b
yumin <green4469@gmail.com>
2018-03-18 16:19:38 +0900

    still round function problem

--- FacilityLocation.h
+++ FacilityLocation.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <cstdlib>
 #include <ilcplex/ilocplex.h>
 #include <random>
 #include <string>
@@ -7,11 +8,10 @@
 #include <vector>       // std::vector
 #include <ctime>        // std::time
 #include <cstdlib>      // std::rand, std::srand
-
 using namespace std;
 
 #define NUM_OF_F 3
-#define NUM_OF_C 10
+#define NUM_OF_C 6
 class FacilityLocation {
 private:
 	/* Rounded problem's objective function's cost */
@@ -30,6 +30,7 @@ private:
 
 	/* exponential clocks of facilities */
 	double exponential_clock[NUM_OF_F];
+
 	/* the order of the exponential clocks of the clients by ascending */
 	int order_of_client[NUM_OF_C];
 
@@ -45,7 +46,7 @@ public:
 	/* constructor, inside it initialize the oppening cost, connection cost, clients' clocks, facilities' clocks */
 	// ÇÐ¼ö
 	FacilityLocation();
-	
+
 	/* solve the LP-relaxed facility location problem */
 	// ¸íÀå
 	double LP_solve();
@@ -53,7 +54,7 @@ public:
 	/* round the LP-relaxed solution to the original problem's solution */
 	// À¯¹Î
 	void round();
-	 
+
 	/* check all possible solutions and pick the minimum cost (brute-force) */
 	// À¯¹Î
 	void brute_force();
@@ -62,7 +63,45 @@ public:
 
 	/* compare LP rounded solution and optimal solution */
 	// ¸íÀå
-	unsigned int objective(bool optimal=0);
+	unsigned int objective(bool optimal = 0);
 
+	int get_optimal_cost() {
+		return this->optimal_cost;
+	}
 
+	int get_rounded_cost() {
+		return this->rounded_cost;
+	}
+
+	unsigned int * get_connection_cost() {
+		return this->connection_cost;
+	}
+
+	unsigned int * get_opening_cost() {
+		return this->opening_cost;
+	}
+
+	bool * get_optimal_connection_table() {
+		return this->optimal_connection_table;
+	}
+
+	bool * get_optimal_opening_table() {
+		return this->optimal_opening_table;
+	}
+
+	double * get_opening_variable() {
+		return this->opening_variable;
+	}
+
+	double * get_connection_variable() {
+		return this->connection_variable;
+	}
+
+	bool * get_opening_table() {
+		return this->opening_table;
+	}
+
+	bool * get_connection_table() {
+		return this->connection_table;
+	}
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,69 @@
 #include "FacilityLocation.h"
 
-int main(void)
-{
-	FacilityLocation ex;
-	ex.LP_solve();
-	ex.random_sample();
-	ex.round();
-	ex.brute_force();
-	ex.objective();
+template <typename T>
+void print_contents(T a, T b);
 
-	return 0;
+int main() {
+	FacilityLocation fl = FacilityLocation();
+	unsigned int * connection_cost = fl.get_connection_cost();
+	unsigned int * opening_cost = fl.get_opening_cost();
+	double * connection_variable;
+	double * opening_variable;
+	bool * opening_table;
+	bool * connection_table;
+
+	cout << "Facilitys' opening costs, connection costs" << endl;
+	print_contents(opening_cost, connection_cost);
+
+
+	/* LP solver sovle the relaxed problem */
+	double sol = fl.LP_solve();
+	cout << "The obj val of relaxation : " << sol << endl;
+	cout << endl;
+
+
+	/* Print LP_solver's output (Opening variables, Connection variables)*/
+	connection_variable = fl.get_connection_variable();
+	opening_variable = fl.get_opening_variable();
+
+	cout << "Facilitys' opening variables, connection variables" << endl;
+	print_contents(opening_variable, connection_variable);
+	
+
+	/* Find optimal solution through brute-force */
+	fl.brute_force();
+	bool* optimal_connection_table = fl.get_optimal_connection_table();
+	bool* optimal_opening_table = fl.get_optimal_opening_table();
+
+	cout << "Facilitys' optimal opening variables, optimal connection variables" << endl;
+	print_contents(optimal_opening_table, optimal_connection_table);
+
+	cout << "The obj val of brute-force alg : " << fl.get_optimal_cost() << endl;
+	cout << endl;
+
+	/* Find rounded solution */
+	fl.round();
+	cout << "The obj val of rounding alg : " << fl.get_rounded_cost() << endl;
+
+	cout << "Facilitys' rounded opening variables, rounded connection variables" << endl;
+	opening_table = fl.get_opening_table();
+	connection_table = fl.get_connection_table();
+	print_contents(opening_table, connection_table);
+
+}
+
+template <typename T>
+void print_contents(T a, T b)
+{
+	for (int i = 0; i < NUM_OF_F; ++i) {
+		cout << "y[" << i << "]: " << a[i] << endl;
+	}
+
+	cout << endl;
+	for (int i = 0; i < NUM_OF_F; ++i) {
+		for (int j = 0; j < NUM_OF_C; ++j) {
+			cout << "x[" << i << "," << j << "]: " << b[i*NUM_OF_C + j] << endl;
+		}
+	}
+	cout << endl;
 }
